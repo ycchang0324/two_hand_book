e8dd2025821c1805f3fe56a10216d8fa7e6f7f6c
Justin Chang <ycchang0324@gmail.com>
2020-06-28 00:47:01 +0800

    buyBook

--- buyBook.php
+++ buyBook.php
@@ -11,48 +11,50 @@ require 'db_connection.php';
 $conn = connection();
 
 $data = array(
-array("微積分",0,0,0,0),
-array("交換電路與邏輯設計",0,0,0,0),
-array("生物科學通論",0,0,0,0),
-array("普通化學丙",0,0,0,0),
-array("普通物理甲",0,0,0,0),
-array('電子學(一)',0,0,0,0),
-array("電磁學(一)",0,0,0,0),
-array("線性代數",0,0,0,0),
-array("微分方程",0,0,0,0),
-array("離散",0,0,0,0),
-array("複變",0,0,0,0),
-array("演算法",0,0,0,0),
-array("資料結構",0,0,0,0)
+    "微積分甲上下"=>array(0,0,0,0),
+    "交換電路與邏輯設計"=>array(0,0,0,0),
+    "生物科學通論"=>array(0,0,0,0),
+    "普通化學丙"=>array(0,0,0,0),
+    "普通物理學甲"=>array(0,0,0,0),
+    "計算機程式設計"=>array(0,0,0,0),
+    "電子學(一)"=>array(0,0,0,0),
+    "電磁學(一)"=>array(0,0,0,0),
+    "工程數學-線性代數"=>array(0,0,0,0),
+    "工程數學-微分方程"=>array(0,0,0,0),
+    "工程數學-離散數學"=>array(0,0,0,0),
+    "工程數學-複變"=>array(0,0,0,0),
+    "演算法"=>array(0,0,0,0),
+    "資料結構"=>array(0,0,0,0)
 );
     
-for($x = 0; $x < 12; $x++){
-    $subject = $data[$x][0];
+    foreach($data as $x => $x_value) {
+        $sql = "SELECT id FROM bookorder WHERE subject = '$x' AND price = 200";
+         $result = $conn->query($sql);
+        echo $x . " ";
+        $data[$x][0] = $result->num_rows;
+        echo $data[$x][0] . " ";
+        
+        
+         $sql = "SELECT id FROM bookorder WHERE subject = '$x' AND price = 300";
+         $result = $conn->query($sql);
+        $data[$x][1] = $result->num_rows;
+        echo $data[$x][1] . " ";
+        
+         $sql = "SELECT id FROM bookorder WHERE subject = '$x' AND price = 500";
+         $result = $conn->query($sql);
+       $data[$x][2] = $result->num_rows;
+        echo $data[$x][2] . " ";
+        
+         $sql = "SELECT id FROM bookorder WHERE subject = '$x' AND price = 700";
+         $result = $conn->query($sql);
+       $data[$x][3] = $result->num_rows;
+        echo $data[$x][3] . "\n";
+    }
     
-    $sql = "SELECT id FROM bookorder WHERE subject = '$subject' AND price = 200";
-     $result = $conn->query($sql);
     
-    $data[$x][1] = $result->num_rows;
+    
+    
 
-    $sql = "SELECT id FROM bookorder WHERE subject = '$subject' AND price = 300";
-    $result = $conn->query($sql);
-    
-    
-    $data[$x][2] = $result->num_rows;
-    
-    $sql = "SELECT id FROM bookorder WHERE subject = '$subject' AND price = 500";
-    $result = $conn->query($sql);
-    
-    
-    $data[$x][3] = $result->num_rows;
-    
-    $sql = "SELECT id FROM bookorder WHERE subject = '$subject' AND price = 700";
-    $result = $conn->query($sql);
-    
-    
-    $data[$x][4] = $result->num_rows;   
-
-}
 
     json_encode(["data"=>$data]);
 
