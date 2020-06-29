d39d8377b6df33bce7ee12c42dc88fdbf88393fa
Justin Chang <ycchang0324@gmail.com>
2020-06-29 19:48:44 +0800

    login

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require_once 'db_connection.php';
+require 'manage.php';
+require_once 'confirm_mailer.php';
+
+$data = json_decode(file_get_contents("php://input"));
+$account = $data -> account;
+$password = $data -> password;
+
+$manager = new Manage($account,$password);
+$success = $manager->login();
+
+json_encode(["success"=>$success]);
+
+
+
+?>
