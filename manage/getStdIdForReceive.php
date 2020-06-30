58e99d43e0394bcefb5e61a6b7a7add526d18a4f
Justin Chang <ycchang0324@gmail.com>
2020-06-30 12:15:55 +0800

    manage file

---
+++ manage/getStdIdForReceive.php
@@ -0,0 +1,21 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require_once '../db_connection.php';
+require '../manage.php';
+require_once '../confirm_mailer.php';
+
+$data = json_decode(file_get_contents("php://input"));
+$stdId = $data -> stdId;
+
+$manager = new Manage();
+$manager->receiveBookStdId($stdId);
+
+
+
+
+?>
