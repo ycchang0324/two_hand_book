52b06dcbbf73504c7498fd6264d2ce568df534a2
Justin Chang <ycchang0324@gmail.com>
2020-07-04 21:51:17 +0800

    feedback

--- db/db_create.php
+++ db/db_create.php
@@ -25,7 +25,7 @@ $conn = new mysqli($servername,$username,$password,$dbname);
 // sql to create table
 
 
-$sql = "CREATE TABLE BookOrder (
+$sql = "CREATE TABLE bookorder (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30) NOT NULL,
 stdId VARCHAR(30) NOT NULL,
@@ -46,7 +46,7 @@ if ($conn->query($sql) === TRUE) {
 
 
 
-$sql = "CREATE TABLE Seller (
+$sql = "CREATE TABLE seller (
 stdId INT(6) NOT NULL,
 name VARCHAR(30) NOT NULL,
 bookNum INT(2) NOT NULL,
@@ -60,7 +60,7 @@ if ($conn->query($sql) === TRUE) {
 }
 
 
-$sql = "CREATE TABLE Login (
+$sql = "CREATE TABLE login (
 id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 account VARCHAR(30) NOT NULL,
 password VARCHAR(30) NOT NULL,
@@ -73,10 +73,37 @@ if ($conn->query($sql) === TRUE) {
   //echo "Error creating table: " . $conn->error;
 }
 
-$sql = "CREATE TABLE Email (
+$sql = "CREATE TABLE oldbook (
+id INT(5) NOT NULL,
+name VARCHAR(100) NOT NULL,
+category VARCHAR(30) NOT NULL,
+price INT(5) NOT NULL,
+picture INT(5) NOT NULL,
+isSold INT(2) NOT NULL
+)";
+
+if ($conn->query($sql) === TRUE) {
+  //echo "Table Seller created successfully";
+} else {
+  //echo "Error creating table: " . $conn->error;
+}
+
+$sql = "CREATE TABLE email (
 
 account VARCHAR(30) NOT NULL,
 password VARCHAR(30) NOT NULL
+)";
+
+if ($conn->query($sql) === TRUE) {
+  //echo "Table Seller created successfully";
+} else {
+  //echo "Error creating table: " . $conn->error;
+}
+
+$sql = "CREATE TABLE feedback (
+id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+comment VARCHAR(500) NOT NULL,
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 
 if ($conn->query($sql) === TRUE) {
---
+++ feedback.php
@@ -0,0 +1,40 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+require './db/db_connection.php';
+require_once './class/mail.php';
+
+// POST DATA
+
+$data = json_decode(file_get_contents("php://input"));
+
+
+
+$comment = $data -> comment;
+
+$conn = connection();
+
+$sql = "INSERT INTO feedback (comment)
+VALUES ('$feedback')";
+
+if ($conn->query($sql) === TRUE) {
+    $sql = "SELECT * FROM feedback ";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0){
+            $allComment = $result -> fetch_all(MYSQLI_ASSOC);
+            echo json_encode(["success"=>1,"allComment"=>$allComment],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
+        }
+        else{
+            echo json_encode(["success"=>0]);
+        }
+  
+} else {
+  echo json_conde(["success"=>0]);
+}
+
+$conn->close();
+?>
