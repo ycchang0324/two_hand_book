6d3cf2e69c7cb4f59b5799303ee9e5b64619934a
Justin Chang <ycchang0324@gmail.com>
2020-07-08 17:09:44 +0800

    manage

--- class/manage.php
+++ class/manage.php
@@ -2,10 +2,8 @@
 
 
 class Manage {
-  
-
-    
-    function receiveBookStdId($stdId){
+      
+    function getBookOrderStdId($stdId){
         $conn = connection();
         $sql = "SELECT * FROM bookorder WHERE stdId = '$stdId' AND state = '未收到書'";
         $result = $conn->query($sql);
@@ -20,37 +18,21 @@ class Manage {
         
     }
     
-    function isReceive($id){
+    function getBookOrderBookId($id){
         $conn = connection();
-        $sql = "UPDATE bookorder SET state='已收到書' WHERE id='$id' AND state = '未收到書'";
-
-    if ($conn->query($sql) === TRUE) {
-          echo json_encode(["success"=>1,"msg"=>"成功收書"],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
-        
-        $mailer = new Mailer;
-        $sql = "SELECT * FROM bookorder WHERE id='$id'";
+        $sql = "SELECT * FROM bookorder WHERE id = '$id' AND state = '未收到書'";
         $result = $conn->query($sql);
-        
-        if($result->num_rows > 0)
-             $orderList = $result -> fetch_all(MYSQLI_ASSOC);
-        
-        $mailer -> sellerSetMail( 
-            $orderList[0][stdId], 
-            $orderList[0][name], 
-            $orderList[0][subject], 
-            $orderList[0][price] 
-        );
-        $mailer->sendMailReceive();
-        
-          
-        } else {
-            $msg = "收書登記失敗: " . $conn->error;
-          echo json_encode(["success"=>0,"msg"=>"$msg"],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
-        
+        if($result->num_rows > 0){
+            $orderList = $result -> fetch_all(MYSQLI_ASSOC);
+            echo json_encode(["success"=>1,"orderList"=>$orderList],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
         }
-        
+        else{
+            echo json_encode(["success"=>0]);
+        }
         $conn->close();
+        
     }
+    
     function notReceive(){
         $conn = connection();
         $sql = "SELECT * FROM bookorder WHERE state = '未收到書'";
@@ -73,42 +55,6 @@ class Manage {
         $conn->close();
     }
     
-    function sendMailNotReceive(){
-        $mailer = new Mailer;
-        $mailer->sendMailNotReceive();
-    }
-    
-    
-    
-    function buyBookToFront($id){
-        $conn = connection();
-        $sql = "SELECT * FROM bookorder WHERE id='$id' AND state = '已收到書'";
-        $result = $conn->query($sql);
-        if($result->num_rows > 0){
-            $orderList = $result -> fetch_all(MYSQLI_ASSOC);
-            echo json_encode(["success"=>1,"orderList"=>$orderList],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
-        }
-        else{
-            echo json_encode(["success"=>0]);
-        }
-        
-        $conn->close();
-    }
-    
-    function isSold($id){
-        $conn = connection();
-        $sql = "UPDATE bookorder SET state='已賣出' WHERE id='$id' AND state = '已收到書'";
-
-    if ($conn->query($sql) === TRUE) {
-          echo json_encode(["success"=>1,"msg"=>"成功賣書"],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
-        } else {
-          $msg = "賣書失敗" . $conn->error;
-                echo json_encode(["success"=>0,"msg"=>"$msg"],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
-        }
-        
-        $conn->close();
-    }
-    
     function notSold(){
         $conn = connection();
         $sql = "SELECT * FROM bookorder WHERE state = '已收到書'";
@@ -125,59 +71,6 @@ class Manage {
         }
 
         $conn->close(); 
-    }
-    
-    function sendSellingResult(){
-        $mailer = new Mailer;
-        $mailer->sendSellingResult();
-
-        $conn->close();
-        
-    }
-    
-    function givenBackStdId($stdId){
-        $conn = connection();
-        $sql = "SELECT * FROM bookorder WHERE stdId = '$stdId' AND (state = '已賣出' or state = '沒賣出')";
-        $result = $conn->query($sql);
-        if($result->num_rows > 0){
-            $orderList = $result -> fetch_all(MYSQLI_ASSOC);
-            echo json_encode(["success"=>1,"orderList"=>$orderList],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
-        }
-        else{
-            echo json_encode(["success"=>0]);
-        }
-        $conn->close();
-        
-    }
-    
-    function isGivenBack($id){
-        $conn = connection();
-        $sql = "UPDATE bookorder SET state='已領錢或退書' WHERE id='$id'";
-
-    if ($conn->query($sql) === TRUE) {
-            echo json_encode(["success"=>1,"msg"=>"成功領錢或退書"],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
-        
-            $mailer = new Mailer;
-            $sql = "SELECT * FROM bookorder WHERE id='$id'";
-            $result = $conn->query($sql);
-
-            if($result->num_rows > 0)
-                 $orderList = $result -> fetch_all(MYSQLI_ASSOC);
-
-            $mailer -> sellerSetMail( 
-                $orderList[0][stdId], 
-                $orderList[0][name], 
-                $orderList[0][subject], 
-                $orderList[0][price] 
-            );
-            $mailer->sendMailGivenBack();
-            
-            }else {
-                $msg = "更改狀態失敗 " . $conn->error;
-                echo json_encode(["success"=>0,"msg"=>"$msg"],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
-            }
-        
-        $conn->close();
     }
     
     function notGivenBack(){
@@ -199,26 +92,13 @@ class Manage {
         
     }
     
-    function sendMailNotGivenBack(){
-        $mailer = new Mailer;
-        $mailer -> sendMailNotGivenBack();
-    }
+
     
+   
+
     
-    function showNotGivenBack(){
-        $conn = connection();
-        $sql = "SELECT * FROM bookorder WHERE state = '已賣出' OR state = '沒賣出'";
-        $result = $conn->query($sql);
-        if($result->num_rows > 0){
-            $orderList = $result -> fetch_all(MYSQLI_ASSOC);
-            echo json_encode(["success"=>1,"orderList"=>$orderList],JSON_UNESCAPED_UNICODE,JSON_FORCE_OBJECT);
-        }
-        else{
-            echo json_encode(["success"=>0]);
-        }
-        $conn->close();
-        
-    }
+
+
     
     
     
